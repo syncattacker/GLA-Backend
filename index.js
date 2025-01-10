dba619ac6a57c30d1ce07bdb550e9172a4fa8cc3
syncattacker <cybereyerahul@gmail.com>
2025-01-10 11:51:26 +0530

    Message

--- index.js
+++ index.js
@@ -3,10 +3,20 @@ const path = require("path");
 
 const PATH = path.join(`${__dirname}/data.txt`);
 
-fs.writeFile(PATH, "Learning Node Js File System Moye Moye", (error) => {
+const newLine = "This is the new text to be written";
+
+fs.writeFile(PATH, newLine, (error) => {
   if (!error) {
     console.log("Written Successfully");
   } else {
     console.log(error);
   }
 });
+
+// fs.writeFile(PATH, "Learning Node Js File System Moye Moye", (error) => {
+//   if (!error) {
+//     console.log("Written Successfully");
+//   } else {
+//     console.log(error);
+//   }
+// });
